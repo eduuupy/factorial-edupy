8efbb9ed86acc122430dee2ed56434d6c86571c3
eduuupy <1793986@alu.murciaeduca.es>
2021-03-20 11:23:29 +0100

    Factorial.java: Identificacion de la rama

--- Factorial.java
+++ Factorial.java
@@ -1,6 +1,8 @@
+// ESTO ES LA RAMA 2
 public class Factorial {
 	/**
 	 * Esto es un comentario
+	 * @param args Los argumentos del programa
 	 */
 	public static void Main (String[] args) {
 		boolean sabeProgramar = false
